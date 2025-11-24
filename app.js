951a4761c258b48a83801460db710cf31702133a
Losange2 <bosslosange@gmail.com>
2025-11-24 12:22:12 +0100

    ouais

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+    // Enregistrement du Service Worker
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js')
+            .then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(function(error) {
+                console.log('Service Worker registration failed:', error);
+            });
+    }
+
     let allbeer = [];
     let currentPage = 1;
     let perPage = 10;
